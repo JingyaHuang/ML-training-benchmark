d527b7575adadb85fe09e0a430e4490de173165b
JingyaHuang <huang_jingya@outlook.com>
2022-03-18 02:22:10 +0100

    For pytorch tests

--- run.sh
+++ run.sh
@@ -5,8 +5,9 @@ CMD=${1:-/bin/bash}
 export CUDA_VISIBLE_DEVICES=4
 
 # Install dependencies
-pip install coloredlogs transformers>=4.15.0 datasets>=1.8.0
+pip install coloredlogs transformers>=4.15.0 datasets>=1.18.0
 pip install scipy sklearn
 
 # Run the example
-python -m unittest onnxruntime/test_examples.py
+# python -m unittest onnxruntime/test_examples.py
+python -m unittest pytorch/test_pytorch_examples.py
